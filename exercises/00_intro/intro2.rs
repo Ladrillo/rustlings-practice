bb1dddcc05f1fc89bda57cddf1b0608aa2b77e0f
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-04-13 12:25:48 +0200

    initial

---
+++ exercises/00_intro/intro2.rs
@@ -0,0 +1,10 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.va a pagar 50K euros.
+
+fn main() {
+    println!("Hello there!")
+}
