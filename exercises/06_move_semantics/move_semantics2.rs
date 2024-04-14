df20780d628135aa049f89bfbf1d958611b4a05d
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-04-14 16:48:08 +0200

    wip

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -11,16 +11,16 @@
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(&vec0);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let &mut vec: &Vec<i32> = vec;
 
     vec.push(88);
 
-    vec
+    vec.to_vec()
 }
